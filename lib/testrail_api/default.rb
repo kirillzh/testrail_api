a6fd4cc053b29416e7149d07e570676ce98c9b37
Kirill Zhukov <zhukov.kirill.96@gmail.com>
2016-01-06 13:45:54 -0800

    Added TestRail::Default module with default values

---
+++ lib/testrail_api/default.rb
@@ -0,0 +1,16 @@
+module TestRail
+  module Default
+    # Default User-Agent header
+    USER_AGENT ||= "TestRail API Ruby Gem #{TestRail::VERSION}".freeze
+
+    # Default media type
+    MEDIA_TYPE ||= 'application/json'.freeze
+
+    # Default headers
+    HEADERS    ||= {
+        'Content-Type' => MEDIA_TYPE,
+        # 'Accept'       => MEDIA_TYPE,
+        'User-Agent'   => USER_AGENT
+    }.freeze
+  end
+end
